d56ffa810e4499cc3adef1e2471b15dd960f1945
Jay-Dee Daniels <jydiecpt@gmail.com>
2025-06-16 21:40:51 +0200

    Update script.js in Challenge 2-d.

--- Challenge 2/Challenge d/script.js
+++ Challenge 2/Challenge d/script.js
@@ -1,5 +1,5 @@
-import company form 'configuration'
-import year form 'configuration'
+import { company, year } from 'configuration';
 
-const message = '© ' + company + ' (' + year + ')'
-document.querySelector('footer').innerText = message
+const message = '© ' + company + ' (' + year + ')';
+document.querySelector('footer').innerText = message;
+
